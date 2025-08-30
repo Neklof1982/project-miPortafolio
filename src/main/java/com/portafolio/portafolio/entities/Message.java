e504eecce7bae62454a2edf5c250c1dce7546ea8
Neklof1982 <neklof1982@hotmail.com>
2025-08-30 20:51:47 +0200

    Entities y Enum

---
+++ src/main/java/com/portafolio/portafolio/entities/Message.java
@@ -0,0 +1,34 @@
+package com.portafolio.portafolio.entities;
+
+import java.time.LocalDateTime;
+
+import com.portafolio.portafolio.enums.MessageStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String email;
+    private String contenido;
+    private LocalDateTime fechaEnvio;
+    private MessageStatus status;
+
+}
---
+++ src/main/java/com/portafolio/portafolio/enums/MessageStatus.java
@@ -0,0 +1,9 @@
+package com.portafolio.portafolio.enums;
+
+public enum MessageStatus {
+
+    RECIBIDO, 
+    RESPONDIDO,
+    FINALIZDO
+    
+}
